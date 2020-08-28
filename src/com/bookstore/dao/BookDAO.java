6c01db64791ca1f68ef706e8c76f44bc97ab5a17
swrajapp <69860620+swrajapp@users.noreply.github.com>
2020-08-28 09:10:14 +0530

    Update BookDAO.java

--- src/com/bookstore/dao/BookDAO.java
+++ src/com/bookstore/dao/BookDAO.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.javaguides.usermanagement.model.User;
+import java.bookmanagement.model.Book;
 
 
 public class BookDAO {
